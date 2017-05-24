02ff8571f3f8c1d7896199451ecb37da8467d129
xpchina2003 <xpchina@xpchina2003deiMac.local>
2017-05-24 16:20:55 +0800

    realm

---
+++ Realm实践/Realm实践/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Realm实践
+//
+//  Created by xpchina2003 on 2017/5/24.
+//  Copyright © 2017年 xpchina2003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Realm实践/Realm实践/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Realm实践
+//
+//  Created by xpchina2003 on 2017/5/24.
+//  Copyright © 2017年 xpchina2003. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
